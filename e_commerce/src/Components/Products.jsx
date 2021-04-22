2ca5d0744c3780329144efa986c338d9d5eaf79b
Johnathan <jonathanumana86@gmail.com>
2021-04-22 14:26:19 -0400

    Made Product File just for the Products image,title & more

--- e_commerce/src/Components/Products.jsx
+++ e_commerce/src/Components/Products.jsx
@@ -11,7 +11,9 @@ const Products = () => {
 
         <Grid container justify="center"spacing={4}>
             {products.map((product) => (
-                <Grid item key={product.id}>
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product />
+                </Grid>
             ))}
 
         </Grid>
