3e4e9b717a36b42a324157af972295bac77d7ebe
Johnathan <jonathanumana86@gmail.com>
2021-04-22 15:09:22 -0400

    Makig the cards for Product

--- e_commerce/src/Components/Product.jsx
+++ e_commerce/src/Components/Product.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Card,  CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
+import classes from '*.module.css'
 
 export const Product = () => {
     return (
-        <Card>
-            
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={Product.name} />
+
+            <CardContent>
+                <div className={classes.CardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {Product.name}
+                    </Typography>
+                </div>
+            </CardContent>
+
         </Card>
     )
 }
