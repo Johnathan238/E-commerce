f091c389cb3d8609aba63e04593d57d136c55b16
Johnathan <jonathanumana86@gmail.com>
2021-05-01 15:57:43 -0400

    Making Navbar now

---
+++ e_commerce/src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography }
+import { ShoppingCart } from '@material-ui/icons'
+import classes from '*.module.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <AppBar position="fixed" className={classes.AppBar} color="inherit">
+                <Toolbar>
+                    <Typography>
+                        <img src={}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
+
+export default Navbar
