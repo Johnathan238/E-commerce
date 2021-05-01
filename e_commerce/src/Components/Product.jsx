0d08c837bfadaa66318ebf5c56f12675864c60f5
Johnathan <jonathanumana86@gmail.com>
2021-05-01 14:43:19 -0400

    Working on style for Product.jsx

--- e_commerce/src/Components/Product.jsx
+++ e_commerce/src/Components/Product.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Card,  CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
 import classes from '*.module.css'
+import useStyles from './styles'
 
 export const Product = ({ product }) => {
+    const classes = useStyles()
+    
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image='' title={Product.name} />
---
+++ e_commerce/src/Components/styles.js
@@ -0,0 +1,8 @@
+import { makeStyle, makeStyles } from '@material-ui/core/styles'
+
+
+export default makeStyles(() => ({
+    root:{
+        maxWidth:'100%'
+    }
+}))
