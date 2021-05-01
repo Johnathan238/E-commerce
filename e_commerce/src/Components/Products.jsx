893131f5ac65fd831767d9ed8a104174f209e313
Johnathan <jonathanumana86@gmail.com>
2021-05-01 17:19:26 -0400

    Made Products style so the products wont hit the Navbar

--- e_commerce/src/Components/Products.jsx
+++ e_commerce/src/Components/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Product from './Product'
+import useStyle from './Productstyle'
 
 const products = [
     { id:1, name:'Shoes', description:'Nike x Acrynom', price:'$50.00', image: 'https://images.unsplash.com/photo-1558004282-daef63d6ac9c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1850&q=80'},
@@ -8,9 +9,10 @@ const products = [
 ];
 
 const Products = () => {
+    const  classes = useStyle()
     return(
-        <main>
-
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
