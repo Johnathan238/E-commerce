7a2cb1c14476a2e0623357beb51fe40bfe3ee7f8
Johnathan <jonathanumana86@gmail.com>
2021-05-01 14:52:03 -0400

    Did some Styles for Product.jsx

--- e_commerce/src/Components/styles.js
+++ e_commerce/src/Components/styles.js
@@ -4,5 +4,20 @@ import { makeStyle, makeStyles } from '@material-ui/core/styles'
 export default makeStyles(() => ({
     root:{
         maxWidth:'100%'
+    },
+
+    media: {
+        height: 0,
+        paddingTop: '56.25%'
+    },
+
+    cardActions: {
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+
+    cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between'
     }
 }))
