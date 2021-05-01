ed4e7a75206b644039abd80238e2504e560f5a3c
Johnathan <jonathanumana86@gmail.com>
2021-05-01 16:21:08 -0400

    Added custom logo and iconbutton for shpping cart

--- e_commerce/src/Components/Navbar.jsx
+++ e_commerce/src/Components/Navbar.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography }
 import { ShoppingCart } from '@material-ui/icons'
 import classes from '*.module.css'
+import logo from '../assets/I AM PRODIGY FITNESS Logo.png'
 
 const Navbar = () => {
     return (
         <div>
             <AppBar position="fixed" className={classes.AppBar} color="inherit">
                 <Toolbar>
-                    <Typography>
-                        <img src={}
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="I AM PRODIGY FITNESS" height="25px" className={classes.image} />
+                        I AM PRODIGY FITNESS
                     </Typography>
+
+                    <div className={classes.grow} />
+                    <div className={classes.button} />
+                    <IconButton aria-label="Show cart items" color="inherit">
+                        <Badge badgeContent={2} color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
                 </Toolbar>
             </AppBar>
         </div>
