db450d4ccdd05c81df4692fee90aa01b5f03f4fd
Johnathan <jonathanumana86@gmail.com>
2021-05-09 16:22:49 -0400

    Making the Cart layout with Material-ui

---
+++ e_commerce/src/Components/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
+
+const Cart = () => {
+    return (
+        <Container>
+            
+            
+        </Container>
+    )
+}
+
+export default Cart
+
