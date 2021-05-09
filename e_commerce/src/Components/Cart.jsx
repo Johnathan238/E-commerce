7ae0e78988b13b08b705139b676c061dbd6116d3
Johnathan <jonathanumana86@gmail.com>
2021-05-09 16:27:05 -0400

    Making if : else for shoppign cart

--- e_commerce/src/Components/Cart.jsx
+++ e_commerce/src/Components/Cart.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
+import classes from '*.module.css'
 
 const Cart = () => {
+    const isEmpty = true;
+
+    
     return (
         <Container>
-            
-            
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} varaint="h3">Your Shopping Cart</Typography>
+            { isEmpty ? <EmptyCard /> : <FilledCart />}
         </Container>
     )
 }
