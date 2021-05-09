11bc1415a4762e17d9f3925e81edb9d86b6615a7
Johnathan <jonathanumana86@gmail.com>
2021-05-09 19:06:13 -0400

    Made checkout and Empty Button

--- e_commerce/src/Components/Cart.jsx
+++ e_commerce/src/Components/Cart.jsx
@@ -22,9 +22,13 @@ const Cart = ({ cart }) => {
 
         <div className={classes.cardDetails}>
             <Typography>
-                Subtotal: {cart.subtotal}
+                Subtotal: {cart.subtotal.formatted_with_symbol}
             </Typography>
+            <div>
+                <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
 
+                <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout Now!</Button>
+            </div>
         </div>
         </>
     }
