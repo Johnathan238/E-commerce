e1708a1c9a5788a6074a6ae58102c112d72d2f12
Johnathan <jonathanumana86@gmail.com>
2021-05-23 21:02:25 -0400

    I finnaly debugged it, took a while but the cart works

--- e_commerce/src/Components/Cart.jsx
+++ e_commerce/src/Components/Cart.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
-import useStyle from './styles'
+import useStyles from './styles'
 import CartItem from './CartItem'
 
-
 const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items.length;
-    const classes = useStyle()
+    // const isEmpty = !cart.line_items.length;
+    const classes = useStyles()
 
-    const EmptyCart = () => {
+    const EmptyCart = () => (
         <Typography variant="subtitle2">No items in your shopping cart!</Typography>
-    }
+    )
 
-    const FilledCart = () => {
+    const FilledCart = () => (
         <>
         <Grid container spacing={3}>
             {cart.line_items.map((item) => (
@@ -33,18 +32,19 @@ const Cart = ({ cart }) => {
             </div>
         </div>
         </>
-    }
+    )
 
     if(!cart.line_items) return 'Loading... ';
 
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart />}
+            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
 }
 
 export default Cart
+
 
