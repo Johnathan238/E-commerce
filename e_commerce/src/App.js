fd4eaf96384b98ee5d13a624cda7fe78203c9ebf
Johnathan <jonathanumana86@gmail.com>
2021-05-30 21:56:12 -0400

    I passed them into cart

--- e_commerce/src/App.js
+++ e_commerce/src/App.js
@@ -68,7 +68,12 @@ const App = () => {
           </Route>
 
           <Route exact path="/cart">
-            <Cart cart={cart} />
+            <Cart 
+            cart={cart} 
+            handleUpdateCartQty={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleEmptyCart={handleEmptyCart}
+            />
           </Route>
         </Switch>
 
