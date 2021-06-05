6253ea3dd4b88b80cee5a9074679fbf35c7c4edd
Johnathan238 <jonathanumana86@gmail.com>
2021-06-05 01:28:57 -0400

    Mapping to get the step

--- e_commerce/src/Components/Checkout.jsx
+++ e_commerce/src/Components/Checkout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import classes from '*.module.css';
 
-
+const steps = ['Payment Details']
 const Checkout = () => {
   return (
     <>
@@ -10,7 +10,13 @@ const Checkout = () => {
       <main className={classes.layout}>
         <Paper className={classes.paper}>
           <Typography variant="h4" align="center">Checkout!🛒</Typography>
-          <Stepper activeStep={0} className={classes.stepper}></Stepper>
+          <Stepper activeStep={0} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
         </Paper>
 
       </main>
