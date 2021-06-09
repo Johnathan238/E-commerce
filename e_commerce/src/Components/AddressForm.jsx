e91df7ffa57a0d4258cd2a74749d133efcb24027
Johnathan238 <jonathanumana86@gmail.com>
2021-06-09 16:21:25 -0400

    Dont need address form yet

--- e_commerce/src/Components/AddressForm.jsx
+++ e_commerce/src/Components/AddressForm.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-const AddressForm = () => {
-  return (
-    <div>
-        AddressForm
-    </div>
-  )
-}
+// const AddressForm = () => {
+//   return (
+//     <div>
+//         AddressForm
+//     </div>
+//   )
+// }
 
-export default AddressForm
+// export default AddressForm
--- e_commerce/src/Components/Checkout.jsx
+++ e_commerce/src/Components/Checkout.jsx
@@ -1,15 +1,23 @@
 import React, {useState} from 'react'
 import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 import useStyles from './Checkoutstyles'
+// import AddressForm from './AddressForm'
+import PaymentForm from './PaymentForm'
 
-const steps = ['Payment Details']
+const steps = ['Payment Details', 'Confim!']
 
 const Checkout = () => {
 
-  const [activeStep, setActiveStep] = useState(0)
+  const [activeStep, setActiveStep] = useState(1)
   const classes = useStyles()
+
+  const Confirm = () => (
+    <>
+     Confirm! 
+    </>
+  )
   
-  const Form = () => activeStep == 0 ? <AddressForm /> : <PaymentForm />
+  const Form = () => activeStep == 1 ? <PaymentForm /> : <Confirm />
   return (
     <>
       <div className={classes.toolbar} />
