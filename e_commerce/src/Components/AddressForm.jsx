c5090f5073eb06601bafea46d7e422263665f617
Johnathan <jonathanumana86@gmail.com>
2021-06-13 17:17:04 -0400

    I will make addressForm and fix the issue Im running into

--- e_commerce/src/Components/AddressForm.jsx
+++ e_commerce/src/Components/AddressForm.jsx
@@ -1,11 +1,19 @@
-// import React from 'react'
+import React from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core'
+import { useFrom, FormProvider } from 'react-hook-form'
 
-// const AddressForm = () => {
-//   return (
-//     <div>
-//         AddressForm
-//     </div>
-//   )
-// }
 
-// export default AddressForm
+const AddressForm = () => {
+    const methods = useForm()
+
+  return (
+    <>
+        <Typography variant="h6" gutterBottom>Name</Typography>
+        <FormProvider {...methods}>
+
+        </FormProvider>
+    </>
+  )
+}
+
+export default AddressForm
