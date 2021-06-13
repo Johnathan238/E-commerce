0eefd91ced622b9a6222bf6c7afd0fdf76548ff7
Johnathan <jonathanumana86@gmail.com>
2021-06-13 18:57:30 -0400

    UseState to get countries from api

--- e_commerce/src/Components/AddressForm.jsx
+++ e_commerce/src/Components/AddressForm.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core'
 import { useForm, FormProvider } from 'react-hook-form'
 import FormInput from './FormInput'
+import { commerce } from '../library/commerce'
+
 
 
 const AddressForm = () => {
     const methods = useForm()
+    const [shippingCountries, setShippingCountries] = useState([])
+
+    const fetchShippingCountires = async (checkoutTokenId) => {
+        const response = await commerce.services.localeListShippingCountries(checkoutTokenId)
+    }
 
   return (
     <>
